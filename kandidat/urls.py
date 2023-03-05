dc2c14704215e0e4f166b93a16e0672c5f11f9a0
ngouana001 <romaricy@yahoo.com>
2023-03-06 00:29:05 +0100

    logout funktion

--- kandidat/urls.py
+++ kandidat/urls.py
@@ -1,6 +1,6 @@
 from kandidat import views
 from django.urls import path, re_path
-from kandidat.views import home,kandidat_add,kandidat_update,kandidat_all,kandidat_home,kandidat_delete,kandidat_delete_all,signin,register
+from kandidat.views import home,kandidat_add,kandidat_update,kandidat_all,kandidat_home,kandidat_delete,kandidat_delete_all,signin,register,signout
 
 urlpatterns = [
     re_path(r'^api/kandidaten$', views.kandidaten_list),
@@ -15,6 +15,6 @@ urlpatterns = [
     path('kandidat/all/delete',kandidat_delete_all,name="kandidat-delete-all"),
     path('login',signin,name='login'),
     path('register',register,name='register'),
-    
+    path('logout',signout,name='logout'),
 
 ]
